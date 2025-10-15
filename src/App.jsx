fc20847a249d61d458c3e15f9b227773a29f12cd
CarolinaVirginio <carolinavir35@gmail.com>
2025-10-15 14:09:40 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
